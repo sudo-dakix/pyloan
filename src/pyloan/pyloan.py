32ff7ef7545f0963a1549c24ec7ee9f19fd693b9
daki <dakix@protonmail.com>
2021-05-06 23:28:27 +0200

    commit on 2021-05-06

--- src/pyloan/pyloan.py
+++ src/pyloan/pyloan.py
@@ -3,6 +3,11 @@
 #import datetime as dt
 #from dateutil.relativedelta import relativedelta
 
+from collections import namedtuple
+from decimal import Decimal
+
+Payment=namedtuple('Payment',['pmt_no','pmt','int','pmt_regular','pmt_special','pmt_total','balance'])
+
 class Loan(object):
 
     def __init__(self,principal,interest,duration,payment_amount,interest_only_period=0,annual_payments=12):
@@ -23,6 +28,26 @@ class Loan(object):
         
         # to delete
         self._parameters={}
+        
+    def amortize(self):
+        pmt_init=Payment(pmt_no=0,pmt=0,int=0,pmt_regular=0,pmt_special=0,pmt_total=0,balance=self.principal)
+        pmt_schedule=[pmt_init]
+        
+        for i in range(1,self.no_of_payments+1):
+            
+            balance_bop=pmt_schedule[i-1].balance
+            amt_interest = 0. if balance_bop == 0. else balance_bop*self.interest*self.compounding_factor
+            amt_principal_regular = self.payment_amount-amt_interest
+            amt_principal_special = 0.
+            amt_principal_total = min(amt_principal_regular+amt_principal_special,balance_bop)
+            amt_total_payment=amt_principal_total + amt_interest
+            
+            balance_eop=balance_bop-amt_principal_total
+            
+            installment=Payment(pmt_no=i,pmt=self.payment_amount,int=amt_interest,pmt_regular=amt_principal_regular,pmt_special=amt_principal_special,pmt_total=amt_total_payment,balance=balance_eop)
+            pmt_schedule.append(installment)
+        
+        return pmt_schedule
         
     #@property
     def set_parameters(self):
